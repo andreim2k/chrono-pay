b711b24d20ce7386ea79836fff3e939fa4f5da3b
Andrei Manz <andrei.manz@gmail.com>
2025-10-29 08:11:02 +0000

    Change the SVG Icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,9 +12,15 @@ export function ChronoPayLogo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
+      {/* Outer circle (timer/clock representation) */}
       <circle cx="12" cy="12" r="10" />
-      <path d="M12 1v22" />
-      <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+
+      {/* Checkmark - positioned left */}
+      <path d="M5.5 12l2 2 3-3" strokeWidth="1.5" />
+
+      {/* Dollar sign - positioned right */}
+      <path d="M15.5 8v8" strokeWidth="1.5" />
+      <path d="M17.5 9.5h-3a1.5 1.5 0 0 0 0 3h2a1.5 1.5 0 0 1 0 3h-3" strokeWidth="1.5" />
     </svg>
   );
 }
