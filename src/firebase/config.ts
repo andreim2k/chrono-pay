1ad9ba1430fd27047879285d4eee72c6df6b2792
andreim2k <andreim2k@users.noreply.github.com>
2026-09-04 10:37:40 +0300

    Configure App Hosting environment variables and firebase config fallback

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || 'AIzaSyAFCw6dcRuRuZCZDcPwtVUO59qeYHSe4-k',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || 'studio-500169941-54f40.firebaseapp.com',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || 'studio-500169941-54f40',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '1:1062624729761:web:049b6c014c98faac58aee0',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '1062624729761',
 };
